e38810caf39c37e77cef2079084681ba7493484f
Victor <victor.yingyi@gmail.com>
2023-11-03 17:47:32 -0700

    add layer0 test

---
+++ test/layer0.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include "rawdisk.h"
+
+int main() {
+    char *d = strdup("/dev/vdb");
+    RawDisk *H = new RawDisk(d);
+    
+    char *buf = "iloveosdfjlseirfnerig";
+    char readBuffer[512] = {0};  // Initialize to zeros
+
+    //printf("dir %s, numSectors %lld, diskSize %lld \n", H->dir, H->numSectors, H->diskSize);
+
+    //use number to substitute H->getnumSector(), getnumSectors() are not yest implemented
+    for(off_t i = 0; i < 10; i++) {
+        H->rawdisk_write(i, buf);
+    }
+    //use number to substitute H->getnumSector(), getnumSectors() are not yest implemented
+    for(off_t i = 0; i < 10; i++) {
+        H->rawdisk_read(i, readBuffer);
+        assert(strncmp(readBuffer, buf, strlen(buf)) == 0);
+    }
+
+    delete H;  // Delete the RawDisk object
+
+    return 0;
+}
