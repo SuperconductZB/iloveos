43d1a57f12900930aad2de651ace5872612b25b5
Victor <victor.yingyi@gmail.com>
2023-11-11 13:55:45 -0800

    delete assign unsigned char

--- test/layer1_API.cpp
+++ test/layer1_API.cpp
@@ -17,35 +17,35 @@ int main(int argc, char *argv[]) {
     H->rawdisk_read((1) * SECTOR_SIZE, buffer, sizeof(buffer));
     u_int64_t t = 0;
     for (int j = 0; j < 8; j++) 
-        t |= ((u_int64_t)(unsigned char)buffer[j]) << (8 * j);
+        t |= ((u_int64_t)buffer[j]) << (8 * j);
 
     assert(t == 2);
     //test the number before end of inode 524286
     H->rawdisk_read((MAX_INODE - 2) * SECTOR_SIZE, buffer, sizeof(buffer));
     t = 0;
     for (int j = 0; j < 8; j++) 
-        t |= ((u_int64_t)(unsigned char)buffer[j]) << (8 * j);
+        t |= ((u_int64_t)buffer[j]) << (8 * j);
 
     assert(t == MAX_INODE - 1);
     //test the end of inode 1~524287
     H->rawdisk_read((MAX_INODE - 1) * SECTOR_SIZE, buffer, sizeof(buffer));
     t = 0;
     for (int j = 0; j < 8; j++) 
-        t |= ((u_int64_t)(unsigned char)buffer[j]) << (8 * j);
+        t |= ((u_int64_t)buffer[j]) << (8 * j);
 
     assert(t == 0);
     //test the begining of datablock
     H->rawdisk_read((MAX_INODE) * SECTOR_SIZE, buffer, sizeof(buffer));
     t = 0;
     for (int j = 0; j < 8; j++) 
-        t |= ((u_int64_t)(unsigned char)buffer[j]) << (8 * j);
+        t |= ((u_int64_t)buffer[j]) << (8 * j);
 
     assert(t == (MAX_INODE+2048*8)*SECTOR_SIZE);
     //test the end of the datablock
     H->rawdisk_read((MAX_BLOCKNUM - 2048*8) * SECTOR_SIZE, buffer, sizeof(buffer));
     t = 0;
     for (int j = 0; j < 8; j++) 
-        t |= ((u_int64_t)(unsigned char)buffer[j]) << (8 * j);
+        t |= ((u_int64_t)buffer[j]) << (8 * j);
 
     assert(t == (MAX_BLOCKNUM)*SECTOR_SIZE);
 
