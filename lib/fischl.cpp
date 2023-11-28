76f5780854520046bc1d284055b59a7ed11dbddd
FactorialN <factorialn@169-231-87-64.wireless.ucsb.edu>
2023-11-28 15:44:56 -0800

    added all callbacks for implementation

--- lib/fischl.cpp
+++ lib/fischl.cpp
@@ -38,22 +38,57 @@ static int fischl_read(const char* path, char *buf, size_t size, off_t offset, s
 }
 
 static const struct fuse_operations fischl_oper = {
-	.init           = fischl_init,
-	.getattr	= fischl_getattr,
-	.readdir	= fischl_readdir,
-	.open		= fischl_open,
-    .mkdir      = fischl_mkdir,
-	.read		= fischl_read,
+	.init        = fischl_init,
+    .destroy     = fischl_destroy,
+    .getattr     = fischl_getattr,
+    .fgetattr    = fischl_fgetattr,
+    .access      = fischl_access,
+    .readlink    = fischl_readlink,
+    .readdir     = fischl_readdir,
+    .mknod       = fischl_mknod,
+    .mkdir       = fischl_mkdir,
+    .symlink     = fischl_symlink,
+    .unlink      = fischl_unlink,
+    .rmdir       = fischl_rmdir,
+    .rename      = fischl_rename,
+    .link        = fischl_link,
+    .chmod       = fischl_chmod,
+    .chown       = fischl_chown,
+    .truncate    = fischl_truncate,
+    .ftruncate   = fischl_ftruncate,
+    .utimens     = fischl_utimens,
+    .create      = fischl_create,
+    .open        = fischl_open,
+    .read        = fischl_read,
+    .write       = fischl_write,
+    .statfs      = fischl_statfs,
+    .release     = fischl_release,
+    .opendir     = fischl_opendir,
+    .releasedir  = fischl_releasedir,
+    .fsync       = fischl_fsync,
+    .flush       = fischl_flush,
+    .fsyncdir    = fischl_fsyncdir,
+    .lock        = fischl_lock,
+    .bmap        = fischl_bmap,
+    .ioctl       = fischl_ioctl,
+    .poll        = fischl_poll,
+#ifdef HAVE_SETXATTR
+    .setxattr    = fischl_setxattr,
+    .getxattr    = fischl_getxattr,
+    .listxattr   = fischl_listxattr,
+    .removexattr = fischl_removexattr,
+#endif
+    .flag_nullpath_ok = 0,
 };
 
 static void fischl::show_help(const char *progname)
 {
 	printf("usage: %s [options] <mountpoint>\n\n", progname);
 	printf("File-system specific options:\n"
-	       "    --name=<s>          Name of the \"hello\" file\n"
-	       "                        (default: \"hello\")\n"
-	       "    --contents=<s>      Contents \"hello\" file\n"
-	       "                        (default \"Hello, World!\\n\")\n"
+	       "    --name=<s>          Name of the \"fischl\" file\n"
+	       "                        (default: \"fischl\")\n"
+	       "    --contents=<s>      Contents \"fischl\" file\n"
+	       "                        (default \"fischl, World!\\n\")\n"
 	       "\n");
 }
 
